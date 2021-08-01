ab0bc409a76fd706c2028ffa1885645b5d4b9ae2
Iamnotagenius <Iamnotagenius@github.com>
2021-08-01 20:34:45 +0500

    for laptop

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 static const unsigned int border_width = 2;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"iosevka:size=14:antialias=true"
+	"iosevka:size=20:antialias=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
