6ecdeb87c11396b958f1e939c3fc0b2713fa5a98
Iamnotagenius <kazakk9@gmail.com>
2021-11-21 13:46:27 +0300

    Alpha!!!

--- config.h
+++ config.h
@@ -2,8 +2,9 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xD0;
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-static const unsigned int border_width = 2;
+static const unsigned int border_width = 0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"iosevka:size=14:antialias=true"
@@ -17,6 +18,13 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSelHighlight] = { "#d3d0c8", "#cc99cc" },
 	[SchemeOut] = {  "#000000", "#00ffff" },
 };
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ VERSION = 5.0
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
+X11INC = /usr/include/X11
 X11LIB = /usr/X11R6/lib
 
 # Xinerama, comment if you don't want it
@@ -20,7 +20,7 @@ FREETYPEINC = /usr/include/freetype2
 
 # includes and libs
 INCS = -I$(X11INC) -I$(FREETYPEINC)
-LIBS = -L$(X11LIB) -lX11 $(XINERAMALIBS) $(FREETYPELIBS) -lm
+LIBS = -L$(X11LIB) -lX11 -lXrender $(XINERAMALIBS) $(FREETYPELIBS) -lm
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS)
